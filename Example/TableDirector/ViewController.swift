ecf1c81b083d58c9f7de2f3e7ea94ddc863a73b8
Aleksandr <aleksios@manychat.com>
2020-04-12 13:43:35 +0300

    Initial commit

---
+++ Example/TableDirector/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  TableDirector
+//
+//  Created by aleksiosdev on 04/12/2020.
+//  Copyright (c) 2020 aleksiosdev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+

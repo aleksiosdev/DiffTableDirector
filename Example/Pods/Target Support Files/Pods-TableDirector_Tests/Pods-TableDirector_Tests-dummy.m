a044c1ca3e291fd36472e6dce6c0794a9ea27d85
Aleksandr <aleksios@manychat.com>
2020-04-12 13:49:21 +0300

    Iitial commit

---
+++ Example/Pods/Target Support Files/Pods-TableDirector_Tests/Pods-TableDirector_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TableDirector_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_TableDirector_Tests
+@end
---
+++ Example/Pods/Target Support Files/TableDirector/TableDirector-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_TableDirector : NSObject
+@end
+@implementation PodsDummy_TableDirector
+@end

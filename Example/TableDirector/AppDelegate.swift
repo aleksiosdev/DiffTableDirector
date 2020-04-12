32003d0a1cef7a0cc281f3f20088f2d17b8c66e8
Aleksandr <aleksios@manychat.com>
2020-04-12 17:17:44 +0300

    Feature: Added storyboard and code controller with cells

--- Example/TableDirector/AppDelegate.swift
+++ Example/TableDirector/AppDelegate.swift
@@ -11,11 +11,29 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    var window: UIWindow?
+	var window: UIWindow?
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        return true
-    }
+	func application(
+		_ application: UIApplication,
+		didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?)
+		-> Bool {
+			let tabBarController = UITabBarController()
+			let storyboardViewController = createStoryboardViewController()
+			storyboardViewController.tabBarItem = UITabBarItem(title: "Storyboard", image: nil, selectedImage: nil)
+			let codeViewController = CodeViewController()
+			codeViewController.tabBarItem = UITabBarItem(title: "Code", image: nil, selectedImage: nil)
+			tabBarController.setViewControllers([storyboardViewController, codeViewController], animated: false)
+
+			let window = UIWindow(frame: UIScreen.main.bounds)
+			window.rootViewController = tabBarController
+			window.makeKeyAndVisible()
+			self.window = window
+			return true
+	}
+
+	func createStoryboardViewController() -> UIViewController {
+		let storyboard = UIStoryboard(name: "Main", bundle: nil)
+		return storyboard.instantiateViewController(withIdentifier: "StoryboardViewController")
+	}
 }
 
---
+++ Example/TableDirector/InfoCell.swift
@@ -0,0 +1,45 @@
+//
+//  InfoCell.swift
+//  TableDirector_Example
+//
+//  Created by Aleksandr Lavrinenko on 12.04.2020.
+//  Copyright © 2020 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+final class InfoCell: UITableViewCell {
+	// MARK: - UI
+	private let _titleLabel = UILabel()
+	private let _descriptionLabel: UILabel = {
+		let titleLabel = UILabel()
+		titleLabel.numberOfLines = 0
+		return titleLabel
+	}()
+
+	// MARK: - Init
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+
+	override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+		super.init(style: style, reuseIdentifier: reuseIdentifier)
+
+		contentView.addSubview(_titleLabel)
+		_titleLabel.translatesAutoresizingMaskIntoConstraints = false
+		[
+			_titleLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8),
+			_titleLabel.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: 16),
+			_titleLabel.rightAnchor.constraint(equalTo: contentView.rightAnchor, constant: 16)
+			].forEach { $0.isActive = true }
+
+		contentView.addSubview(_descriptionLabel)
+		_descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+		[
+			_descriptionLabel.topAnchor.constraint(equalTo: _titleLabel.bottomAnchor, constant: -8),
+			_descriptionLabel.leftAnchor.constraint(equalTo: _titleLabel.leftAnchor),
+			_descriptionLabel.rightAnchor.constraint(equalTo: _titleLabel.rightAnchor),
+			_descriptionLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -8),
+			].forEach { $0.isActive = true }
+	}
+}
